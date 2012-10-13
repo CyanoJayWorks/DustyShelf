54a65093f30682d94335dbaa9669dddffcabe0cc
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-13 16:24:08 -0700

    removing 'welcome' message

--- core/core.php
+++ core/core.php
@@ -1,5 +1,5 @@
 <?php
-	echo '<h1 align=center>Welcome to DustyShelf!</h1>';
+	//echo '<h1 align=center>Welcome to DustyShelf!</h1>';
 	
 	require(DS_PATH . '/core/env_check.php');
 	require(DS_PATH . '/core/autoloader.php');
