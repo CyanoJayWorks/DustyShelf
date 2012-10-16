66c86f3d94532acd461151f699b4c8abf581aa78
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-15 18:50:51 -0700

    improved style compatibilities

--- core/includes/view.php
+++ core/includes/view.php
@@ -12,39 +12,40 @@
         <link rel="stylesheet" type="text/css" href="./core/includes/style.css"/>
     </head>
     <body>
-    	
-	    <div class="header">
-			<a href="<?php $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; echo $url;?>"> 
-				<?php echo Config::get('blog_title') ?>
-			</a>
-			
-			<h4><?php echo Config::get('blog_title_desc') ?></h4>
-			<img src="<?php echo Config::get('blog_header_img') ?>" href="<?php echo Config::get('blog_header_img') ?>"/>
-		</div>
-		
-		<div id="content">
-			<div class="blogbody">
-				<?php
-					$results = DB::executeResultQuery('SELECT body,title,tags FROM posts');
-					
-					foreach ($results as $key => $value) {
-						echo '<div class="blogpost">';
-						echo '<div class="blogposttitle">';
-						echo '<h4>' . $value['title'] . '</h4>';
-						echo '</div>';
-						echo '<div class="blogposttag">';
-						echo $value['tags'] . '<hr />';
-						echo '</div>';
-						echo $value['body'];
-						echo '</div>';
-					}
-				?>
+    	<div id="content">
+		    <div class="header">
+				<a href="<?php $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; echo $url;?>"> 
+					<?php echo Config::get('blog_title') ?>
+				</a>
+				
+				<h4><?php echo Config::get('blog_title_desc') ?></h4>
 			</div>
 			
-			<div class="sidebar">
-				<?php
-					echo Config::get('blog_desc'); 
-				?>
+			<div id="blogcontent">
+				<img src="<?php echo Config::get('blog_header_img') ?>" href="<?php echo Config::get('blog_header_img') ?>"/>
+				<div class="blogbody">
+					<?php
+						$results = DB::executeResultQuery('SELECT body,title,tags FROM posts');
+						
+						foreach ($results as $key => $value) {
+							echo '<div class="blogpost">';
+							echo '<div class="blogposttitle">';
+							echo '<h4>' . $value['title'] . '</h4>';
+							echo '</div>';
+							echo '<div class="blogposttag">';
+							echo $value['tags'] . '<hr />';
+							echo '</div>';
+							echo $value['body'];
+							echo '</div>';
+						}
+					?>
+				</div>
+				
+				<div class="sidebar">
+					<?php
+						echo Config::get('blog_desc'); 
+					?>
+				</div>
 			</div>
 		</div>
 		
@@ -52,6 +53,6 @@
 			<hr />
 			<p> <?php echo Config::get('blog_cpyrt'); ?></p>
 		</div>
-
+		
     </body>
 </html>
