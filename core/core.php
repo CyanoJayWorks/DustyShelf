820299306b3f1e32f7c33249290c518e01aa4894
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-19 19:29:57 -0700

    cleaning up unnecessary Config metadata storage

--- core/core.php
+++ core/core.php
@@ -18,13 +18,6 @@
 		
 	}
 	
-	Config::set('blog_nav_title', 'Ajay\'s Blog');
-	Config::set('blog_title', 'Ajay\'s Blog');
-	Config::set('blog_title_desc', 'The one and only');
-	Config::set('blog_desc', 'An awesome blog about awesome stuff.');
-	Config::set('blog_cpyrt', '&copy; 2012 Ajay Gopinath. All rights reserved.');
-	Config::set('blog_header_img', './core/assets/banner.png');
-	
 	DB::connect(Config::get('mysql_host'), Config::get('mysql_user'), Config::get('mysql_pass'));
 	DB::selectDatabase('dustyshelf');
 	
