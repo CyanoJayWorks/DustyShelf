947782cf07216a62588896b3e7e6a3c3d18456a4
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-20 18:52:05 -0700

    abandoning cross-compatibility between hosts

--- core/core.php
+++ core/core.php
@@ -10,25 +10,13 @@
 						  'mysql_pass',
 					      'mysql_host', 
 					      'mysql_port'))) {
-					      	
-		$isPHPFog = getenv('MYSQL_HOST');
-			      	
-		if(!empty($isPHPFog)) {	      		 	
-			Config::set('mysql_user', getenv('MYSQL_USERNAME'));
-			Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
-			Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
-			Config::set('mysql_port', '3306');
-			Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
-		} else {
-			Config::set('mysql_user', 'root');
-			Config::set('mysql_pass', '');
-			Config::set('mysql_host', 'localhost');
-			Config::set('mysql_port', '3306');
-			Config::set('mysql_db', 'dustyshelf');
-		}
+
+		Config::set('mysql_user', getenv('MYSQL_USERNAME'));
+		Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
+		Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
+		Config::set('mysql_port', '3306');
+		Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
 	}
-						  
-	echo Config::get('mysql_host');
 	
 	DB::connect(Config::get('mysql_host'), Config::get('mysql_user'), Config::get('mysql_pass'));
 	DB::selectDatabase(Config::get('mysql_db'));
