9e290ff697ea80c7c1bca23c49e1c8c7106018dd
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-20 18:06:24 -0700

    reinstating core.php

---
+++ core/core.php
@@ -0,0 +1,33 @@
+<?php
+	//echo '<h1 align=center>Welcome to DustyShelf!</h1>';
+	
+	require(DS_PATH . '/core/env_check.php');
+	require(DS_PATH . '/core/autoloader.php');
+	
+	spl_autoload_register('dustyshelf_class_autoload');
+	
+	if(!Config::hasKeys(array('mysql_user',
+						  'mysql_pass',
+					      'mysql_host', 
+					      'mysql_port'))) {
+					      		 	
+		Config::set('mysql_user', getenv('MYSQL_USERNAME'));
+		Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
+		Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
+		Config::set('mysql_port', '3306');
+		Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
+	}
+	
+	DB::connect(Config::get('mysql_host'), Config::get('mysql_user'), Config::get('mysql_pass'));
+	DB::selectDatabase(Config::get('mysql_db'));
+	
+	//BlogInfo::refreshInfo();
+	
+	loadOthers();
+	
+	require_once(DS_PATH . '/core/includes/view.php');
+	
+	DB::close();
+	//$indexController = new IndexController();
+	//$indexController->invokeController();
+?>
