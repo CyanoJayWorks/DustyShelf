b2f1686b99f3ba01bc5c12d14c143ef03db7d5ff
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-20 17:23:27 -0700

    removed DB config debug output

--- core/core.php
+++ core/core.php
@@ -11,15 +11,15 @@
 					      'mysql_host', 
 					      'mysql_port'))) {
 					      		 	
-		Config::set('mysql_user', 'root');
-		Config::set('mysql_pass', '');
-		Config::set('mysql_host', 'localhost');
+		Config::set('mysql_user', getenv('MYSQL_USERNAME'));
+		Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
+		Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
 		Config::set('mysql_port', '3306');
-		
+		Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
 	}
 	
 	DB::connect(Config::get('mysql_host'), Config::get('mysql_user'), Config::get('mysql_pass'));
-	DB::selectDatabase('dustyshelf');
+	DB::selectDatabase(Config::get('mysql_db'));
 	
 	//BlogInfo::refreshInfo();
 	
