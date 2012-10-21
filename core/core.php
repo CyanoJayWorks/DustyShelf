09e20ed49346dd8de3c311e872b5cc82a9d152bb
Ajay Gopinath <ajgopi124@gmail.com>
2012-10-20 18:39:15 -0700

    fixing CSS not loading on rewrite in phpfog + made compatible locally and phpfog

--- core/core.php
+++ core/core.php
@@ -10,12 +10,20 @@
 						  'mysql_pass',
 					      'mysql_host', 
 					      'mysql_port'))) {
-					      		 	
-		Config::set('mysql_user', getenv('MYSQL_USERNAME'));
-		Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
-		Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
-		Config::set('mysql_port', '3306');
-		Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
+					      	
+		if(!getenv('MYSQL_HOST') == '') {	      		 	
+			Config::set('mysql_user', getenv('MYSQL_USERNAME'));
+			Config::set('mysql_pass', getenv('MYSQL_PASSWORD'));
+			Config::set('mysql_host', getenv('MYSQL_DB_HOST'));
+			Config::set('mysql_port', '3306');
+			Config::set('mysql_db', getenv('MYSQL_DB_NAME'));
+		} else {
+			Config::set('mysql_user', 'root');
+			Config::set('mysql_pass', '');
+			Config::set('mysql_host', 'localhost');
+			Config::set('mysql_port', '3306');
+			Config::set('mysql_db', 'dustyshelf');
+		}
 	}
 	
 	DB::connect(Config::get('mysql_host'), Config::get('mysql_user'), Config::get('mysql_pass'));
